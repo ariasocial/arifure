3904a1f75a9be066d521459bf8b9778a2bc40b96
A.R.1a <k.umi1209.alice@gmail.com>
2025-06-17 03:49:26 +0900

    Create sw.js

---
+++ source-code/source/sw/h5.g123.jp/pwa/arifure/sw.js
@@ -0,0 +1,20 @@
+self.RUN_ENV = "production";
+self.version = "20250613-e311500";
+self.appCode = "arifure";
+self.precacheAssets = [{
+    "url": "https://platform-sc.g123.jp/game/production/assets/app-CpUCrGhP.js",
+    "revision": null
+}, {
+    "url": "https://platform-sc.g123.jp/game/production/assets/game-beb9f26b--5uYc-_c.js",
+    "revision": null
+}, {
+    "url": "https://platform-sc.g123.jp/game/production/assets/contextTrackingHelper-nB-OPn1T.css",
+    "revision": null
+}, {
+    "url": "https://platform-sc.g123.jp/game/production/assets/game-03165520-C9c3cPxP.js",
+    "revision": null
+}, {
+    "url": "https://platform-sc.g123.jp/game/production/assets/game-d8b296a6-D6-XlEtG.js",
+    "revision": null
+}];
+importScripts('https://platform-sc.g123.jp/game/production/assets/sw.2a4e21a7.js');

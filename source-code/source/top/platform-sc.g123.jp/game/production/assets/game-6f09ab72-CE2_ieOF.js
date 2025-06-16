47eb5bff3ec746de90a4689757c48a67c8659e0c
A.R.1a <k.umi1209.alice@gmail.com>
2025-06-17 03:01:32 +0900

    Create game-6f09ab72-CE2_ieOF.js

---
+++ source-code/source/top/platform-sc.g123.jp/game/production/assets/game-6f09ab72-CE2_ieOF.js
@@ -0,0 +1,593 @@
+import {z as a, u as s, N as re, P as ae, Q as oe, b as se, V as ie, W as ce, d as le, A as ue, y as L, G as fe, H as $, X as pe, j as de, J as g, S as ge, O as u} from "./game-03165520-C9c3cPxP.js";
+import {g as me, r as he, s as ve, _ as i} from "./game-beb9f26b--5uYc-_c.js";
+var P, N, T;
+function h() {
+    return h = Object.assign ? Object.assign.bind() : function(e) {
+        for (var t = 1; t < arguments.length; t++) {
+            var n = arguments[t];
+            for (var r in n)
+                ({}).hasOwnProperty.call(n, r) && (e[r] = n[r])
+        }
+        return e
+    }
+    ,
+    h.apply(null, arguments)
+}
+var we = function(e) {
+    return a("svg", h({
+        xmlns: "http://www.w3.org/2000/svg",
+        width: 24,
+        height: 24,
+        fill: "none"
+    }, e), P || (P = a("circle", {
+        cx: 12,
+        cy: 12,
+        r: 12,
+        fill: "#89B5B8"
+    })), N || (N = a("circle", {
+        cx: 12.444,
+        cy: 8.444,
+        r: 4.444,
+        fill: "#E3FF34"
+    })), T || (T = a("path", {
+        fill: "#E3FF34",
+        fillRule: "evenodd",
+        d: "M4 20.478a8.893 8.893 0 0 1 16.985 0A11.96 11.96 0 0 1 12.493 24 11.96 11.96 0 0 1 4 20.478",
+        clipRule: "evenodd"
+    })))
+}, S, j, A;
+function v() {
+    return v = Object.assign ? Object.assign.bind() : function(e) {
+        for (var t = 1; t < arguments.length; t++) {
+            var n = arguments[t];
+            for (var r in n)
+                ({}).hasOwnProperty.call(n, r) && (e[r] = n[r])
+        }
+        return e
+    }
+    ,
+    v.apply(null, arguments)
+}
+var _e = function(e) {
+    return a("svg", v({
+        xmlns: "http://www.w3.org/2000/svg",
+        width: 40,
+        height: 40,
+        fill: "none"
+    }, e), S || (S = a("circle", {
+        cx: 20,
+        cy: 20,
+        r: 20,
+        fill: "#89B5B8"
+    })), j || (j = a("circle", {
+        cx: 20.407,
+        cy: 14.074,
+        r: 7.407,
+        fill: "#E3FF34"
+    })), A || (A = a("path", {
+        fill: "#E3FF34",
+        fillRule: "evenodd",
+        d: "M6 34.13c1.87-6.04 7.5-10.426 14.154-10.426S32.438 28.09 34.308 34.13A19.94 19.94 0 0 1 20.154 40 19.94 19.94 0 0 1 6 34.13",
+        clipRule: "evenodd"
+    })))
+}, C, I, D, M, k, F;
+function w() {
+    return w = Object.assign ? Object.assign.bind() : function(e) {
+        for (var t = 1; t < arguments.length; t++) {
+            var n = arguments[t];
+            for (var r in n)
+                ({}).hasOwnProperty.call(n, r) && (e[r] = n[r])
+        }
+        return e
+    }
+    ,
+    w.apply(null, arguments)
+}
+var ye = function(e) {
+    return a("svg", w({
+        xmlns: "http://www.w3.org/2000/svg",
+        width: 24,
+        height: 24,
+        fill: "none"
+    }, e), C || (C = a("circle", {
+        cx: 12,
+        cy: 12,
+        r: 12,
+        fill: "#E3FF34"
+    })), I || (I = a("path", {
+        fill: "#262626",
+        d: "M3.875 20.83C5.63 18.454 8.614 16.89 12 16.89s6.37 1.565 8.125 3.942q-.493.453-1.034.85c-1.484-2.057-4.062-3.459-7.091-3.459s-5.607 1.402-7.091 3.46q-.541-.398-1.034-.851Z"
+    })), D || (D = a("path", {
+        fill: "#000",
+        fillRule: "evenodd",
+        d: "M12 15.111a5.333 5.333 0 1 0 0-10.667 5.333 5.333 0 0 0 0 10.667m0 1.333a6.667 6.667 0 1 0 0-13.333 6.667 6.667 0 0 0 0 13.333",
+        clipRule: "evenodd"
+    })), M || (M = a("path", {
+        fill: "#262626",
+        d: "M10.667 9.333a.889.889 0 1 1-1.778 0 .889.889 0 0 1 1.778 0m4.444 0a.889.889 0 1 1-1.778 0 .889.889 0 0 1 1.778 0"
+    })), k || (k = a("path", {
+        fill: "#262626",
+        fillRule: "evenodd",
+        d: "M11.111 13.333a.444.444 0 1 0 0-.889.444.444 0 0 0 0 .89Zm0 .89a1.333 1.333 0 1 0 0-2.667 1.333 1.333 0 0 0 0 2.666Z",
+        clipRule: "evenodd"
+    })), F || (F = a("path", {
+        fill: "#262626",
+        fillRule: "evenodd",
+        d: "M6.667 12.444h3.555v.89H6.667a.444.444 0 0 1 0-.89",
+        clipRule: "evenodd"
+    })))
+}, V, U, B, H, G, q;
+function _() {
+    return _ = Object.assign ? Object.assign.bind() : function(e) {
+        for (var t = 1; t < arguments.length; t++) {
+            var n = arguments[t];
+            for (var r in n)
+                ({}).hasOwnProperty.call(n, r) && (e[r] = n[r])
+        }
+        return e
+    }
+    ,
+    _.apply(null, arguments)
+}
+var Oe = function(e) {
+    return a("svg", _({
+        xmlns: "http://www.w3.org/2000/svg",
+        width: 40,
+        height: 40,
+        fill: "none"
+    }, e), V || (V = a("circle", {
+        cx: 20,
+        cy: 20,
+        r: 20,
+        fill: "#E3FF34"
+    })), U || (U = a("path", {
+        fill: "#262626",
+        d: "M6.458 34.718c2.925-3.962 7.899-6.57 13.542-6.57s10.617 2.608 13.541 6.57q-.821.757-1.723 1.418c-2.473-3.43-6.77-5.766-11.818-5.766-5.05 0-9.345 2.337-11.818 5.766a20 20 0 0 1-1.724-1.418"
+    })), B || (B = a("path", {
+        fill: "#000",
+        fillRule: "evenodd",
+        d: "M20 25.185a8.889 8.889 0 1 0 0-17.777 8.889 8.889 0 0 0 0 17.777m0 2.222c6.137 0 11.111-4.974 11.111-11.11 0-6.137-4.974-11.112-11.111-11.112-6.136 0-11.111 4.975-11.111 11.111S13.864 27.407 20 27.407",
+        clipRule: "evenodd"
+    })), H || (H = a("path", {
+        fill: "#262626",
+        d: "M17.778 15.556a1.481 1.481 0 1 1-2.963 0 1.481 1.481 0 0 1 2.963 0m7.407 0a1.481 1.481 0 1 1-2.963 0 1.481 1.481 0 0 1 2.963 0"
+    })), G || (G = a("path", {
+        fill: "#262626",
+        fillRule: "evenodd",
+        d: "M18.518 22.222a.74.74 0 1 0 0-1.481.74.74 0 0 0 0 1.481m0 1.482a2.222 2.222 0 1 0 0-4.445 2.222 2.222 0 0 0 0 4.445",
+        clipRule: "evenodd"
+    })), q || (q = a("path", {
+        fill: "#262626",
+        fillRule: "evenodd",
+        d: "M11.111 20.74h5.926v1.482h-5.926a.74.74 0 1 1 0-1.481Z",
+        clipRule: "evenodd"
+    })))
+}, J, K, z;
+function y() {
+    return y = Object.assign ? Object.assign.bind() : function(e) {
+        for (var t = 1; t < arguments.length; t++) {
+            var n = arguments[t];
+            for (var r in n)
+                ({}).hasOwnProperty.call(n, r) && (e[r] = n[r])
+        }
+        return e
+    }
+    ,
+    y.apply(null, arguments)
+}
+var be = function(e) {
+    return a("svg", y({
+        xmlns: "http://www.w3.org/2000/svg",
+        width: 24,
+        height: 24,
+        fill: "none"
+    }, e), J || (J = a("circle", {
+        cx: 12,
+        cy: 12,
+        r: 12,
+        fill: "#8C8C8C",
+        opacity: .4
+    })), K || (K = a("circle", {
+        cx: 12.244,
+        cy: 8.444,
+        r: 4.444,
+        fill: "#fff"
+    })), z || (z = a("path", {
+        fill: "#fff",
+        fillRule: "evenodd",
+        d: "M3.6 20.478a8.893 8.893 0 0 1 16.985 0A11.96 11.96 0 0 1 12.092 24c-3.317 0-6.32-1.346-8.492-3.522",
+        clipRule: "evenodd"
+    })))
+}, W, Z, Y;
+function O() {
+    return O = Object.assign ? Object.assign.bind() : function(e) {
+        for (var t = 1; t < arguments.length; t++) {
+            var n = arguments[t];
+            for (var r in n)
+                ({}).hasOwnProperty.call(n, r) && (e[r] = n[r])
+        }
+        return e
+    }
+    ,
+    O.apply(null, arguments)
+}
+var xe = function(e) {
+    return a("svg", O({
+        xmlns: "http://www.w3.org/2000/svg",
+        width: 40,
+        height: 40,
+        fill: "none"
+    }, e), W || (W = a("circle", {
+        cx: 20,
+        cy: 20,
+        r: 20,
+        fill: "#8C8C8C",
+        opacity: .4
+    })), Z || (Z = a("circle", {
+        cx: 20.407,
+        cy: 14.074,
+        r: 7.407,
+        fill: "#fff"
+    })), Y || (Y = a("path", {
+        fill: "#fff",
+        fillRule: "evenodd",
+        d: "M6 34.13c1.87-6.04 7.5-10.426 14.154-10.426S32.438 28.09 34.308 34.13A19.94 19.94 0 0 1 20.154 40 19.94 19.94 0 0 1 6 34.13",
+        clipRule: "evenodd"
+    })))
+};
+const Re = ({type: e="default", size: t="default"}) => s(e === "colorful" ? t === "small" ? we : _e : e === "cs" ? t === "small" ? ye : Oe : t === "small" ? be : xe, {});
+function He(e) {
+    return {
+        render: function(t) {
+            ae(t, e)
+        },
+        unmount: function() {
+            re(e)
+        }
+    }
+}
+var Ee = /&(?:amp|#38|lt|#60|gt|#62|apos|#39|quot|#34|nbsp|#160|copy|#169|reg|#174|hellip|#8230|#x2F|#47);/g
+  , Le = {
+    "&amp;": "&",
+    "&#38;": "&",
+    "&lt;": "<",
+    "&#60;": "<",
+    "&gt;": ">",
+    "&#62;": ">",
+    "&apos;": "'",
+    "&#39;": "'",
+    "&quot;": '"',
+    "&#34;": '"',
+    "&nbsp;": " ",
+    "&#160;": " ",
+    "&copy;": "©",
+    "&#169;": "©",
+    "&reg;": "®",
+    "&#174;": "®",
+    "&hellip;": "…",
+    "&#8230;": "…",
+    "&#x2F;": "/",
+    "&#47;": "/"
+}
+  , $e = function(t) {
+    return Le[t]
+}
+  , Pe = function(t) {
+    return t.replace(Ee, $e)
+};
+function Q(e, t) {
+    var n = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+        var r = Object.getOwnPropertySymbols(e);
+        t && (r = r.filter(function(o) {
+            return Object.getOwnPropertyDescriptor(e, o).enumerable
+        })),
+        n.push.apply(n, r)
+    }
+    return n
+}
+function X(e) {
+    for (var t = 1; t < arguments.length; t++) {
+        var n = arguments[t] != null ? arguments[t] : {};
+        t % 2 ? Q(Object(n), !0).forEach(function(r) {
+            se(e, r, n[r])
+        }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(n)) : Q(Object(n)).forEach(function(r) {
+            Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(n, r))
+        })
+    }
+    return e
+}
+var b = {
+    bindI18n: "languageChanged",
+    bindI18nStore: "",
+    transEmptyNodeValue: "",
+    transSupportBasicHtmlNodes: !0,
+    transWrapTextNodes: "",
+    transKeepBasicHtmlNodesFor: ["br", "strong", "i", "p"],
+    useSuspense: !0,
+    unescape: Pe
+}, te, Ge = oe();
+function Ne() {
+    var e = arguments.length > 0 && arguments[0] !== void 0 ? arguments[0] : {};
+    b = X(X({}, b), e)
+}
+function qe() {
+    return b
+}
+var Je = function() {
+    function e() {
+        ce(this, e),
+        this.usedNamespaces = {}
+    }
+    return ie(e, [{
+        key: "addUsedNamespaces",
+        value: function(n) {
+            var r = this;
+            n.forEach(function(o) {
+                r.usedNamespaces[o] || (r.usedNamespaces[o] = !0)
+            })
+        }
+    }, {
+        key: "getUsedNamespaces",
+        value: function() {
+            return Object.keys(this.usedNamespaces)
+        }
+    }]),
+    e
+}();
+function Te(e) {
+    te = e
+}
+function Ke() {
+    return te
+}
+var ze = {
+    type: "3rdParty",
+    init: function(t) {
+        Ne(t.options.react),
+        Te(t)
+    }
+};
+function Se({langDisplay: e, onNo: t, onOk: n}) {
+    const r = "inline-block w-[68px] leading-[24px] text-xs rounded";
+    return s("div", {
+        className: "fixed   top-[8px] left-0 right-0  sm:top-[20px] sm:left-auto sm:right-[20px]",
+        children: s("div", {
+            className: "w-[298px] bg-surface-primary rounded shadow-[0px_0px_30px_0px_rgba(0,0,0,0.05)] overflow-hidden m-auto",
+            children: [s("div", {
+                className: "pl-[16px] pt-[14px]",
+                children: ["Translate to ", e, "?"]
+            }), s("div", {
+                className: "flex justify-end gap-4 mt-[24px] pr-lg",
+                children: [s("button", {
+                    type: "button",
+                    className: `${r} bg-[#F0F0F0] text-text-primary`,
+                    onClick: t,
+                    children: "No"
+                }), s("button", {
+                    type: "button",
+                    className: `${r} bg-blue-600 text-white`,
+                    onClick: n,
+                    children: "Translate"
+                })]
+            }), s("div", {
+                className: "bg-[#F0F0F0] pl-md leading-[26px] text-text-secondary text-xs mt-md",
+                children: "AI Translate"
+            })]
+        })
+    })
+}
+function je(e) {
+    return new Intl.DisplayNames([e],{
+        type: "language"
+    }).of(e) || e
+}
+function E() {
+    return window.option.appId
+}
+async function Ae(e, t) {
+    try {
+        return (await pe.get(`${de.SHD_G123_WEB_URL}/api/v1/translator/check`, {
+            params: {
+                appCode: e,
+                browserLang: t
+            }
+        })).data.result || []
+    } catch (n) {
+        return window?.Sentry?.captureException(n),
+        []
+    }
+}
+async function Ce() {
+    const e = E()
+      , t = navigator.language || "en"
+      , n = Ve()
+      , r = ne(e);
+    if (t !== n && t !== r) {
+        const o = await Ae(e, t);
+        if (o.length > 0)
+            return o[0]
+    }
+    return ""
+}
+function We({onFinish: e}) {
+    const [t,n] = le("")
+      , r = E()
+      , o = ue(e);
+    L( () => {
+        o.current = e
+    }
+    ),
+    L( () => {
+        let p = !1;
+        async function m() {
+            const d = await Ce();
+            if (!d) {
+                o.current();
+                return
+            }
+            await new Promise(l => {
+                fe().setTimeout(l, 1e3 * 60)
+            }
+            ),
+            !p && n(d)
+        }
+        return m(),
+        () => {
+            p = !0
+        }
+    }
+    , []);
+    const c = () => {
+        $("misc_game_translator_yes", {
+            data: {
+                mtLang: t
+            }
+        }),
+        R(r, "yes"),
+        ke(r, t),
+        window.postMessage(JSON.stringify({
+            action: "reload"
+        }), "*")
+    }
+      , f = () => {
+        $("misc_game_translator_no", {
+            data: {
+                mtLang: t
+            }
+        }),
+        R(r, "no"),
+        o.current()
+    }
+    ;
+    return t ? s(Se, {
+        langDisplay: je(t),
+        onOk: c,
+        onNo: f
+    }) : null
+}
+const x = "g123_game_translator_DJAxg";
+function R(e, t) {
+    const n = localStorage.getItem(x)
+      , r = n ? JSON.parse(n) : {};
+    r[e] = {
+        prevAction: t,
+        prevActionTime: Date.now()
+    },
+    localStorage.setItem(x, JSON.stringify(r))
+}
+function Ie(e) {
+    R(e, "switchToPreset")
+}
+const De = 7 * 24 * 3600 * 1e3;
+function Me() {
+    const e = localStorage.getItem(x);
+    if (!e)
+        return !0;
+    const t = JSON.parse(e)[E()];
+    return t ? t.prevAction === "no" || t.prevAction === "switchToPreset" ? Date.now() - t.prevActionTime > De : !1 : !0
+}
+function Ze() {
+    return Me()
+}
+function Ye(e, t=!1) {
+    if (!e)
+        return;
+    if (g.changeLanguage(e),
+    g.hasResourceBundle(e, "")) {
+        console.error(`[i18n] ${e} is not supported`);
+        return
+    }
+    g.language !== e && g.changeLanguage(e);
+    try {
+        localStorage.setItem(`${ge.pwaSavedLangPrefix}:${window.option.appId}`, e)
+    } catch (o) {
+        console.error(o)
+    }
+    const n = new URL(window.location.href)
+      , r = n.searchParams.get("lang");
+    if (t) {
+        const o = window.option.appId;
+        ne(o) && (Fe(o),
+        Ie(o)),
+        n.searchParams.set("lang", e),
+        window.location.href = n.href
+    }
+    r !== e && (n.searchParams.set("lang", e),
+    r ? window.location.href = n.href : window.history.replaceState(null, "", n.href))
+}
+function ke(e, t) {
+    const n = `gp_mtl_${e}`;
+    ve(n, t, 365)
+}
+function Fe(e) {
+    he(`gp_mtl_${e}`)
+}
+function ne(e) {
+    return me(`gp_mtl_${e}`)
+}
+function Ve() {
+    return new URL(window.location.href).searchParams.get("lang")
+}
+const Qe = e => {
+    const {type: t, content: n} = e;
+    t === "login" ? u.success(n, {
+        icon: s(Re, {
+            size: "small",
+            type: "colorful"
+        })
+    }) : t === "success" ? u.success(n) : t === "error" ? u.error(n) : t === "warning" ? u.warn(n) : u.default(n)
+}
+  , ee = {
+    en: {
+        translation: () => i( () => import("./game-81b3dea9-lz4T-iVD.js"), [])
+    },
+    ja: {
+        translation: () => i( () => import("./game-03cd3bbf-DesnULos.js"), [])
+    },
+    "zh-TW": {
+        translation: () => i( () => import("./game-176866ea-D948KWjZ.js"), [])
+    },
+    "zh-CN": {
+        translation: () => i( () => import("./game-fd5b6bac-Ci2VZDE5.js"), [])
+    },
+    ko: {
+        translation: () => i( () => import("./game-1a56043d-COclUEOJ.js"), [])
+    },
+    fr: {
+        translation: () => i( () => import("./game-b969a660-D_ddRAxa.js"), [])
+    },
+    de: {
+        translation: () => i( () => import("./game-fc500ecb-TjMCJcKM.js"), [])
+    },
+    th: {
+        translation: () => i( () => import("./game-6f1a139a-Bidd5IPx.js"), [])
+    },
+    vi: {
+        translation: () => i( () => import("./game-3af34f6a-DLeB4fwW.js"), [])
+    }
+};
+async function Xe(e, t) {
+    const n = {
+        [e]: ee[e],
+        [t]: ee[t]
+    }
+      , r = {}
+      , o = [];
+    for (const [c,f] of Object.entries(n))
+        if (f)
+            for (const [p,m] of Object.entries(f)) {
+                const d = m().then(l => {
+                    r[c] || (r[c] = {}),
+                    r[c][p] = l
+                }
+                ).catch(l => {
+                    console.error(l)
+                }
+                );
+                o.push(d)
+            }
+    return await Promise.all(o),
+    r
+}
+export {We as G, Ge as I, Je as R, qe as a, Ze as b, He as c, Qe as d, Re as e, Ke as g, ze as i, Xe as l, Ye as s};

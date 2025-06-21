f683c959019b84c380a15abe3a579da180dba2b3
A.R.1a <k.umi1209.alice@gmail.com>
2025-06-21 16:55:52 +0900

    Delete gp-calculator/directAccessRedirect.js

--- gp-calculator/directAccessRedirect.js
+++
@@ -1,7 +0,0 @@
-if (window.top === window.self) {
-    // 例1: 直接アクセスされた場合、専用のエラーページにリダイレクト
-    // ここにリダイレクトしたいURLを記述してください
-    window.location.href = "https://w.atwiki.jp/arifure/pages/48.html";
-    // 例2: コンテンツを完全に非表示にする（リダイレクトしない場合）
-    // document.documentElement.style.display = 'none';
-}
